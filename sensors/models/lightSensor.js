1c07afe480282fe17532a354672d6e9752d324b0
Bryant <bryantsarabia@gmail.com>
2024-10-23 00:15:45 +0200

    refactor: Improve formatting and error handling in LightSensor

--- sensors/models/lightSensor.js
+++ sensors/models/lightSensor.js
@@ -1,42 +1,40 @@
-import { MEASURE_UNITS } from '../consts/measureUnits.js'
-import { SENSOR_TYPES } from '../consts/sensorType.js'
-import { Sensor } from './sensor.js'
+import { MEASURE_UNITS } from "../consts/measureUnits.js";
+import { SENSOR_TYPES } from "../consts/sensorType.js";
+import { Sensor } from "./sensor.js";
 
 export class LightSensor extends Sensor {
-  constructor ({ room, id, value = 0, maxValue }) {
-    const type = SENSOR_TYPES.LIGHT
-    const measureUnit = MEASURE_UNITS[type]
-    super({ type: SENSOR_TYPES.LIGHT, room, id, value, measureUnit })
-    this.maxValue = maxValue
+  constructor({ room, id, value = 0, maxValue }) {
+    const type = SENSOR_TYPES.LIGHT;
+    const measureUnit = MEASURE_UNITS[type];
+    super({ type: SENSOR_TYPES.LIGHT, room, id, value, measureUnit });
+    this.maxValue = maxValue;
   }
 
-  generateData () {
-    const probability = 50
-    const randomProbability = Math.random() * 100
+  generateData() {
+    const probability = 50;
+    const randomProbability = Math.random() * 100;
     if (randomProbability > probability) {
-      const by = Math.floor(Math.random() * 100)
-      const shouldIncrease = (Math.random() * 100) >= 50
+      const by = Math.floor(Math.random() * 100);
+      const shouldIncrease = Math.random() * 100 >= 20;
       this.value = shouldIncrease
         ? Math.min(this.value + by, this.maxValue)
-        : Math.max(this.value - by, 0)
+        : Math.max(this.value - by, 0);
     }
-    return this.value
+    return this.value;
   }
 
-  onActuatorMessage (topic, message) {
-    const shouldProceed = super.onActuatorMessage(topic, message)
-    if (!shouldProceed) return
-    let payload = null
+  onActuatorMessage(topic, message) {
+    const shouldProceed = super.onActuatorMessage(topic, message);
+    if (!shouldProceed) return;
+    let payload = null;
     try {
-      if (message) payload = JSON.parse(message.toString())
-      let { value } = payload
-      value = Number(value)
-      this.value += value
-      if (this.value < 0) this.value = 0
-      if (this.value > this.maxValue) this.value = this.maxValue
-      this.publishData()
-    } catch (error) {
-
-    }
+      if (message) payload = JSON.parse(message.toString());
+      let { value } = payload;
+      value = Number(value);
+      this.value += value;
+      if (this.value < 0) this.value = 0;
+      if (this.value > this.maxValue) this.value = this.maxValue;
+      this.publishData();
+    } catch (error) {}
   }
 }
